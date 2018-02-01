62fd639c12b6069d12c873006f52f2778fba69a8
Jonathan <jkrone@vt.edu>
2018-01-31 20:41:01 -0500

    feat: `ipfs version` flags and `ipfs repo version` (#1199)

--- src/core/components/repo.js
+++ src/core/components/repo.js
@@ -1,13 +1,32 @@
 'use strict'
 
+const repoVersion = require('ipfs-repo').repoVersion
+
 module.exports = function repo (self) {
   return {
     init: (bits, empty, callback) => {
       // 1. check if repo already exists
     },
 
+    /**
+     * If the repo has been initialized, report the current version.
+     * Otherwise report the version that would be initialized.
+     *
+     * @param {function(Error, Number)} [callback]
+     * @returns {undefined}
+     */
     version: (callback) => {
-      self._repo.version.get(callback)
+      self._repo._isInitialized(err => {
+        if (err) {
+          if (/ENOENT|not yet initialized/.test(err.message)) {
+            // this repo has not been initialized
+            return callback(null, repoVersion)
+          }
+          return callback(err)
+        }
+
+        self._repo.version.get(callback)
+      })
     },
 
     gc: function () {},
